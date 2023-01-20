3b2d5c26239b9589b510c4d8a7a54f901dc9b29a
Rodrigo Lourenço <lourero1@hotmail.com>
2023-01-19 22:31:05 -0300

    ajustado arquivos

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect('mongodb://localhost/manager_profile'),
+            mongoose.connect('mongodb://localhost/profiles_manager'),
     },
 ];
