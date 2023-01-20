27eaa5348a762742c4a46cab825f1fd1e8518f1d
Rodrigo Lourenço <lourero1@hotmail.com>
2023-01-20 15:06:31 -0300

    Adicionado validations no DTO

--- src/contents/dto/new-content.input.ts
+++ src/contents/dto/new-content.input.ts
@@ -1,13 +1,19 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class NewContentInput {
+    @IsString()
     @Field({ nullable: false })
+    @MaxLength(30)
     name: string;
 
+    @IsString()
     @Field({ nullable: false })
     description: string;
 
+    @IsString()
+    @MaxLength(30)
     @Field({ nullable: false })
     type: string;
 }
